3995b794eaa2793ddc16cdd9572af9abfa6391d0
j.espada <ctw00684@criticaltechworks.com>
2020-12-12 14:30:11 +0000

    day 3 and day 4 begin

--- day-1/puzzle2.py
+++ day-1/puzzle2.py
@@ -28,9 +28,29 @@ def main2(filename):
                 if bin_search(content, w) != -1:
                     return w*x*y
 
+def main3(filename):
+    content = read_file(filename)
+    m = {}
+    for x in content: 
+        for y in content:
+            if x == y:
+                continue
+            z = x + y
+            if z <= 2020:
+                w = 2020 - z 
+                m[w] = (x,y)              
+    for n in content:
+        if n in m:
+            x,y = m[n]
+            return n * x * y
+    
+
 if __name__ == "__main__":
     n = main("input-1.txt")
     print (n)
     
     n = main2("input-1.txt")
+    print (n)
+
+    n = main3("input-1.txt")
     print (n)
